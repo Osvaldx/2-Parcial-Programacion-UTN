09997f6515a2b375237547ed49f182389009f89b
NsRomano <148826319+NsRomano@users.noreply.github.com>
2024-06-19 19:42:40 -0300

    Delete Clase08 directory

--- Clase08/asd.py
+++
@@ -1,9 +0,0 @@
-matriz_colectivos = [[100, 0, 0, 0, 0, 0],
-                     [98, 0, 0, 0, 0, 0],
-                     [10, 0, 0, 0, 0, 0]]
-
-matriz_colectivos = {100:[1, 2, 3],
-                     98:[1, 2, 3],
-                     10:[1, 2, 3]}
-
-print(matriz_colectivos[98])
