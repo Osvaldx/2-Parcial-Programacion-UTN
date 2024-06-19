b21e9646a4012d7f8253fa80c9f37fe4a7fa59a1
NsRomano <148826319+NsRomano@users.noreply.github.com>
2024-06-19 19:42:34 -0300

    Delete Clase07 directory

--- Clase07/matrices.py
+++
@@ -1,9 +0,0 @@
-
-matriz = [[0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8]]
-
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        print(matriz[i][j], end=" | ")
-    print("")
--- Clase07/matriz_vacia.py
+++
@@ -1,4 +0,0 @@
-matriz = [[0] * 5 for _ in range(4)]
-
-for i in range(len(matriz)):
-    print(matriz[i])
